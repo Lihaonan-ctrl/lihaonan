804322324ba06db4ffa8c1b53b867b3d8b06ebea
Lihaonan-ctrl <2229740274@qq.com>
2025-11-08 18:30:30 +0800

    Refactor array input and reverse logic

--- 7_.c
+++ 7_.c
@@ -2,19 +2,24 @@
 //2229740274@qq.com
 //李浩楠
 #include <stdio.h>
-int main() {
-    int arr[5], num;
-    for (int i = 0; i < 5; i++) {
-        do {
-            scanf("%d", &num);
-        } while (num % 2 != 0); 
-        arr[i] = num;
+
+void reverseArray(int arr[], int len) {
+    for (int i = 0; i < len / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[len - 1 - i];
+        arr[len - 1 - i] = temp;
     }
+}
+
+int main() {
+    int arr[5];
     for (int i = 0; i < 5; i++) {
-        if (i > 0) {
-            printf(" ");
-        }
-        printf("%d", arr[i]);
+        scanf("%d", &arr[i]);
+    }
+    reverseArray(arr, 5);
+    for (int i = 0; i < 5; i++) {
+        if (i == 0) printf("%d", arr[i]);
+        else printf(" %d", arr[i]);
     }
     printf("\n");
     return 0;
