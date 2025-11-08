bf14175606e4a9caf739ecbafcc8eb2246f2c219
Lihaonan-ctrl <2229740274@qq.com>
2025-11-08 18:26:39 +0800

    Implement matrix input functionality
    Add a program to read a 3x3 matrix from user input.

---
+++ 1_.c
@@ -0,0 +1,18 @@
+//202510306218
+//2229740274@qq.com
+//李浩楠
+
+#include<stdio.h>
+
+int main(){
+    int mat[3][3];
+
+    for (int i=0;i<3;i++){
+        scanf("%d %d %d",&mat[i][0],&mat[i][1],&mat[i][2]);
+    }
+
+    for ( int i=0;i<3;i++){
+        scanf("%d %d %d",&mat[i][0],&mat[i][1],&mat[i][2]);
+    }
+    return 0;
+}
