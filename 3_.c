49860333add902b21e44e67b724590e734c005c8
Lihaonan-ctrl <2229740274@qq.com>
2025-11-08 18:28:04 +0800

    Refactor program to sort and print an array
    Replaced prime number checking with array sorting and printing.

--- 3_.c
+++ 3_.c
@@ -1,27 +1,26 @@
 //202510306218
 //2229740274@qq.com
 //李浩楠
-#include <stdio.h>
-int main() {
-    int n, is_prime = 1, i = 2;
-    scanf("%d", &n);
-    
-    if (n <= 1) {
-        is_prime = 0;
-    } else {
-        while (i < n) {
-            if (n % i == 0) {
-                is_prime = 0;
-                break;
-            }
-            i++; 
+#include<stdio.h>
+int main(){
+    int arr[10];
+
+for (int i=0;i<10;i++){
+    scanf("%d",&arr[i]);
+}
+
+for (int i=0;i<9;i++){
+    for(int j =0;j<9-i;j++){
+        if(arr[i]>arr[j+1]){
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
         }
     }
-    
-    if (is_prime) {
-        printf("密钥安全，密码设置成功\n");
-    } else {
-        printf("密钥不安全，请重新输入\n");
-    }
-    return 0;
 }
+
+for(int i=0;i<10;i++){
+    if(i==0)printf("%d",arr[i]);
+    else printf("%d",arr[i]);
+}
+printf("\n");
